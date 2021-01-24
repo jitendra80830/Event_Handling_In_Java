1a23c05e34d4737a0333a403a3bfa7b0cd07e92e
jitendra <jitendrakumar89973@gmail.com>
2021-01-24 14:21:57 +0530

    Example of MouseListener

---
+++ MouseListenerExample.java
@@ -0,0 +1,35 @@
+//MouseListener Example without applet
+import java.awt.*;
+import java.awt.event.*;
+
+public class MouseListenerExample extends Frame implements MouseListener{
+	Label l;
+	MouseListenerExample(){
+		addMouseListener(this);
+		
+		l=new Label();
+		l.setBounds(50,100,500,100);
+		add(l);
+		setSize(400,400);
+		setLayout(null);
+		setVisible(true);
+	}
+	public void mouseClicked(MouseEvent e){
+		l.setText("Mouse Clicked");
+	}
+	public void mouseEntered(MouseEvent e){
+		l.setText("Mouse Entered");
+	}
+	public void mouseExited(MouseEvent e){
+		l.setText("Mouse Exited");
+	}
+	public void mousePressed(MouseEvent e){
+		l.setText("Mouse Pressed");
+	}
+	public void mouseReleased(MouseEvent e){
+		l.setText("Mouse Released");
+	}
+	public static void main(String[] args){
+		new MouseListenerExample();
+	}
+}
